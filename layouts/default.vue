3570dc2463ab79625d1080b78e48e7fafdef371d
djkloop <1044032256@qq.com>
2024-08-07 12:05:38 +0800

    opt style

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@ import LayoutLeft from './components/LayoutLeft.vue'
 <template>
   <div class="font-sans-regular grid grid-cols-4 gap-x-4 overflow-hidden">
     <LayoutLeft />
-    <div col-span-3 overflow-y-scroll py4 class="scrollbar-none h-[calc(100vh-2rem)]">
+    <div col-span-3 overflow-y-scroll py4 pr4 class="scrollbar-none h-[calc(100vh-2rem)]">
       <slot />
     </div>
   </div>
