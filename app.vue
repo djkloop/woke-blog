636d23e30baec5c754d2ae6110c33a0a5ee62801
djkloop <1044032256@qq.com>
2024-08-08 12:43:06 +0800

    update

--- app.vue
+++ app.vue
@@ -4,29 +4,6 @@ import '~/assets/theme.css'
 
 const colorMode = useColorMode()
 
-useHead({
-  script: [
-    {
-      innerHTML: `
-        import { init } from 'https://unpkg.com/@waline/client@v3/dist/waline.js';
-
-        window.onload = () => {
-          setTimeout(() => {
-            const el = document.getElementById('waline');
-            if (!el) return;
-            init({
-              el: '#waline',
-              serverURL: 'https://waline-woke-blog-comment.vercel.app',
-            });
-          }, 500)
-        }
-      `,
-      type: 'module',
-      key: 'waline-comment-script',
-    },
-  ],
-})
-
 onBeforeMount(() => {
   watchEffect(() => {
     if (colorMode.preference === 'dark') {
--- pages/posts/[...post].vue
+++ pages/posts/[...post].vue
@@ -2,6 +2,29 @@
 const route = useRoute()
 const post = route.params.post as Array<string>
 const path = post.join('/')
+
+useHead({
+  script: [
+    {
+      innerHTML: `
+        import { init } from 'https://unpkg.com/@waline/client@v3/dist/waline.js';
+
+        window.onload = () => {
+          setTimeout(() => {
+            const el = document.getElementById('waline');
+            if (!el) return;
+            init({
+              el: '#waline',
+              serverURL: 'https://waline-woke-blog-comment.vercel.app',
+            });
+          }, 500)
+        }
+      `,
+      type: 'module',
+      key: 'waline-comment-script',
+    },
+  ],
+})
 //
 </script>
 
